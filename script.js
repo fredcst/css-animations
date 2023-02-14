1cd6a00e9ea80a9283cc12a34f1318221f12d50c
Frederic Castro <fred.cst@icloud.com>
2023-02-14 01:55:12 +0100

    menu + some new styles

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ const menuContent = document.querySelector("#menu-content");
 const menuButton = document.querySelector("#nav-icon");
 const menuLinks = document.querySelectorAll("#menu-content a");
 
-menuButton.addEventListener("click", () => { menuContent.classList.toggle("hidden");});
+menuButton.addEventListener("click", () => { menuContent.classList.toggle("hidden"); menuButton.classList.toggle("open");});
 menuLinks.forEach(element => {
-    element.addEventListener("click", () => { menuContent.classList.toggle("hidden"); });
+    element.addEventListener("click", () => { menuContent.classList.toggle("hidden"); menuButton.classList.toggle("open");});
 });
-
-menuButton.addEventListener("click", () => { menuButton.classList.toggle("open");});
