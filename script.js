d0fa30e62431bf04355301fd830b3ba549e432f0
Frederic Castro <fred.cst@icloud.com>
2023-03-02 20:36:07 +0100

    new-version

--- script.js
+++ script.js
@@ -12,18 +12,6 @@ menuLinks.forEach(element => {
     element.addEventListener("click", () => { menuContent.classList.toggle("hidden"); menuButton.classList.toggle("open"); brandLogo.classList.toggle("hidden"); logoImg.classList.toggle("hidden");cont.classList.toggle("hidden2");});
 });
 
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("menu").style.top = "0";
-  } else {
-    document.getElementById("menu").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-}
-
 //animaciones css
 
 const observer = new IntersectionObserver(entries => {
