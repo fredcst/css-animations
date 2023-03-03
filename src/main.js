43ec3b9997306457c253a307fb0d3a929922fccb
Frederic Castro <fred.cst@icloud.com>
2023-03-03 18:22:18 +0100

    cleaning-code

--- src/main.js
+++ src/main.js
@@ -2,14 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-// import './assets/old-script.js'
-
 import './assets/main.css'
-import './assets/old-style.css'
 
 const app = createApp(App)
 
 app.use(router)
 
 app.mount('#app')
-
