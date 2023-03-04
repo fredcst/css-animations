9d27838589236a1127009eb08b9f59e044836019
Frederic Castro <fred.cst@icloud.com>
2023-03-04 19:41:50 +0100

    improvements and off vite-app

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,41 @@
-import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import HistoryView from '../views/HistoryView.vue'
+import TutorialView from '../views/TutorialView.vue'
+import ExamplesView from '../views/ExamplesView.vue'
+import AboutView from '../views/AboutView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/history',
+    name: 'history',
+    component: HistoryView
+  },
+  {
+    path: '/tutorial',
+    name: 'tutorial',
+    component: TutorialView
+  },
+  {
+    path: '/examples',
+    name: 'examples',
+    component: ExamplesView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: AboutView
+  }
+]
 
 const router = createRouter({
-  history: createWebHistory('css-animations'),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/history',
-      name: 'history',
-      component: () => import('../views/HistoryView.vue')
-    },
-    {
-      path: '/tutorial',
-      name: 'tutorial',
-      component: () => import('../views/TutorialView.vue')
-    },
-    {
-      path: '/examples',
-      name: 'examples',
-      component: () => import('../views/ExamplesView.vue')
-    }
-  ]
+  history: createWebHistory(process.env.BASE_URL),
+  routes
 })
 
 export default router
