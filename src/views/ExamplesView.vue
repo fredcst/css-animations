f1ee084f227a7bc916c08fcc7ae912efb61d3c38
Frederic Castro <fred.cst@icloud.com>
2023-03-20 23:14:23 +0100

    final css

--- src/views/ExamplesView.vue
+++ src/views/ExamplesView.vue
@@ -1,88 +1,91 @@
 <template>
-    <section id="example" class="page page4">
-        <div class="page-size-wf">
-            <div class="grid-container">
-              <div class="title">Good exemples: CSS animation</div>
-                <div class="intro">
-                  <p>
-                    Explore the unique ways in which designers are using <strong>CSS animation to enhance user experience</strong> and create visually striking designs on websites.
-                  </p>
-                  <p>
-                    These websites use CSS animation to elegantly display dynamic visual effects, such as fading or sliding transitions, between different sections or elements on the page. Navigation is enhanced through the use of CSS animation to reveal related content, saving space and providing a more engaging experience. 
-                  </p>
-                </div>
-              </div>
-              <div class="grid-container">
-                <div>
-                  <h2>Zipeng Zhu</h2>
-                  <p class="text">This website uses CSS animation to apply different types of transformations to elements. This allows for the creation of dynamic visual effects.</p>
-                  <a href="http://zz-is.it/" target="_blank">Go to Zipeng Zhu website <svg style="display: inline-block; width: 1em; height: 1em;" viewbox="0
-                    0 48 48">
-                    <path d="M36 24c-1.2 0-2 0.8-2 2v12c0 1.2-0.8 2-2 2h-22c-1.2
-                    0-2-0.8-2-2v-22c0-1.2 0.8-2 2-2h12c1.2 0 2-0.8 2-2s-0.8-2-2-2h-12c-3.4
-                    0-6 2.6-6 6v22c0 3.4 2.6 6 6 6h22c3.4 0 6-2.6
-                    6-6v-12c0-1.2-0.8-2-2-2z"></path>
-                    <path d="M43.8 5.2c-0.2-0.4-0.6-0.8-1-1-0.2-0.2-0.6-0.2-0.8-0.2h-12c-1.2
-                    0-2 0.8-2 2s0.8 2 2 2h7.2l-18.6 18.6c-0.8 0.8-0.8 2 0 2.8 0.4 0.4 0.8
-                    0.6 1.4 0.6s1-0.2 1.4-0.6l18.6-18.6v7.2c0 1.2 0.8 2 2 2s2-0.8
-                    2-2v-12c0-0.2 0-0.6-0.2-0.8z"></path>
-                    </svg></a>
-                </div>
-                <div>
-                  <a href="hhttp://zz-is.it/" target="_blank"><img src="@/assets/img/img1-exemplespage.png" alt="First Fresh webpage"></a>
-                </div>
-              </div>
-              <div class="grid-container">
-                <div class="invert-order">
-                  <h2>Zorro Design</h2>
-                  <p class="text">This website uses CSS animation to create a parallax scrolling effect, with the background moving at a different speed than the foreground.</p>
-                  <a href="https://www.zorro.design/" target="_blank">Go to the Zorro Design website <svg style="display: inline-block; width: 1em; height: 1em;" viewbox="0
-                    0 48 48">
-                    <path d="M36 24c-1.2 0-2 0.8-2 2v12c0 1.2-0.8 2-2 2h-22c-1.2
-                    0-2-0.8-2-2v-22c0-1.2 0.8-2 2-2h12c1.2 0 2-0.8 2-2s-0.8-2-2-2h-12c-3.4
-                    0-6 2.6-6 6v22c0 3.4 2.6 6 6 6h22c3.4 0 6-2.6
-                    6-6v-12c0-1.2-0.8-2-2-2z"></path>
-                    <path d="M43.8 5.2c-0.2-0.4-0.6-0.8-1-1-0.2-0.2-0.6-0.2-0.8-0.2h-12c-1.2
-                    0-2 0.8-2 2s0.8 2 2 2h7.2l-18.6 18.6c-0.8 0.8-0.8 2 0 2.8 0.4 0.4 0.8
-                    0.6 1.4 0.6s1-0.2 1.4-0.6l18.6-18.6v7.2c0 1.2 0.8 2 2 2s2-0.8
-                    2-2v-12c0-0.2 0-0.6-0.2-0.8z"></path>
-                    </svg>
-                  </a>
-                </div>
-                <div>
-                  <a href="hhttps://www.zorro.design/" target="_blank"><img src="@/assets/img/img2-exemplespage.png" alt="First Fresh webpage"></a>
-                </div>
-              </div>
-              <div class="grid-container">
-                <div>
-                  <h2>NFT Artwork</h2>
-                  <p class="text">This last website use CSS animation to gradually change the color of an element over time and create visually interesting effects.</p>
-                  <a href="https://introspectabilia.illo.tv/" target="_blank">Go to the NFT Artwork website <svg style="display: inline-block; width: 1em; height: 1em;" viewbox="0
-                    0 48 48">
-                    <path d="M36 24c-1.2 0-2 0.8-2 2v12c0 1.2-0.8 2-2 2h-22c-1.2
-                    0-2-0.8-2-2v-22c0-1.2 0.8-2 2-2h12c1.2 0 2-0.8 2-2s-0.8-2-2-2h-12c-3.4
-                    0-6 2.6-6 6v22c0 3.4 2.6 6 6 6h22c3.4 0 6-2.6
-                    6-6v-12c0-1.2-0.8-2-2-2z"></path>
-                    <path d="M43.8 5.2c-0.2-0.4-0.6-0.8-1-1-0.2-0.2-0.6-0.2-0.8-0.2h-12c-1.2
-                    0-2 0.8-2 2s0.8 2 2 2h7.2l-18.6 18.6c-0.8 0.8-0.8 2 0 2.8 0.4 0.4 0.8
-                    0.6 1.4 0.6s1-0.2 1.4-0.6l18.6-18.6v7.2c0 1.2 0.8 2 2 2s2-0.8
-                    2-2v-12c0-0.2 0-0.6-0.2-0.8z"></path>
-                    </svg>
-                  </a>
-                </div>
-                <div>
-                  <a href="https://introspectabilia.illo.tv/" target="_blank"><img src="@/assets/img/img3-exemplespage.png" alt="First Fresh webpage"></a>
-                </div>
-              </div>
-            </div>
-        </section>
-  </template>
+  <section id="example" class="page page4">
+    <div class="page-size-wf">
+      <div class="grid-container">
+        <div class="title">Good exemples: CSS animation</div>
+        <div class="intro">
+          <p>
+            Explore the unique ways in which designers are using
+            <strong>CSS animation to enhance user experience</strong> and create
+            visually striking designs on websites.
+          </p>
+          <p>
+            These websites use CSS animation to elegantly display dynamic visual
+            effects, such as fading or sliding transitions, between different
+            sections or elements on the page. Navigation is enhanced through the
+            use of CSS animation to reveal related content, saving space and
+            providing a more engaging experience.
+          </p>
+        </div>
+      </div>
+      <div class="grid-container">
+        <div>
+          <h2>Zipeng Zhu</h2>
+          <p class="text">
+            This website uses CSS animation to apply different types of
+            transformations to elements. This allows for the creation of dynamic
+            visual effects.
+          </p>
+          <a href="http://zz-is.it/" target="_blank"
+            >Go to Zipeng Zhu website →
+          </a>
+        </div>
+        <div>
+          <a href="hhttp://zz-is.it/" target="_blank"
+            ><img
+              src="@/assets/img/img1-exemplespage.png"
+              alt="First Fresh webpage"
+          /></a>
+        </div>
+      </div>
+      <div class="grid-container">
+        <div class="invert-order">
+          <h2>Zorro Design</h2>
+          <p class="text">
+            This website uses CSS animation to create a parallax scrolling
+            effect, with the background moving at a different speed than the
+            foreground.
+          </p>
+          <a href="https://www.zorro.design/" target="_blank"
+            >Go to the Zorro Design website →
+          </a>
+        </div>
+        <div>
+          <a href="https://www.zorro.design/" target="_blank"
+            ><img
+              src="@/assets/img/img2-exemplespage.png"
+              alt="First Fresh webpage"
+          /></a>
+        </div>
+      </div>
+      <div class="grid-container">
+        <div>
+          <h2>NFT Artwork</h2>
+          <p class="text">
+            This last website use CSS animation to gradually change the color of
+            an element over time and create visually interesting effects.
+          </p>
+          <a href="https://introspectabilia.illo.tv/" target="_blank"
+            >Go to the NFT Artwork website →
+          </a>
+        </div>
+        <div>
+          <a href="https://introspectabilia.illo.tv/" target="_blank"
+            ><img
+              src="@/assets/img/img3-exemplespage.png"
+              alt="First Fresh webpage"
+          /></a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
 
 <script>
 export default {
   beforeRouteEnter(to, from, next) {
-    next(vm => {
-      vm.$root.imagenFondo = require("@/assets/img/examples_img.png")
+    next((vm) => {
+      vm.$root.imagenFondo = require("@/assets/img/examples_img.png");
     });
   },
 };
